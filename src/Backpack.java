e0809619cdcb275c19cea28980724841bfe15853
travis2861 <cheadge@gmail.com>
2014-03-06 20:01:08 -0800

    Equipment finished Skeleton for backpack laided out

--- src/Backpack.java
+++ src/Backpack.java
@@ -2,5 +2,37 @@
  * Created by Travis on 3/6/14.
  */
 public class Backpack {
+    private Canteen canteen;
+    private Equipment equipment[];
 
+    CinReader sc = new CinReader();
+
+    public Backpack()
+    {
+        canteen = canteen;
+        equipment = equipment;
+    }
+
+    private void addCanteen()
+    {
+        canteen = new Canteen();
+    }
+    private void addEquipment(String name)
+    {
+        equipment = new Equipment[0];
+    }
+
+    private void listEquipment()
+    {
+
+    }
+    private void removeEquipment(int removeID)
+    {
+
+    }
+
+    private void removeCanteen()
+    {
+
+    }
 }
--- src/Equipment.java
+++ src/Equipment.java
@@ -2,4 +2,60 @@
  * Created by Travis on 3/6/14.
  */
 public class Equipment {
+
+    private String name;
+    private int weight;
+
+    public Equipment(String cName)
+    {
+        name = validEquip(cName);
+        weight = calcWeight(name);
+    }
+
+    private String validEquip(String name)
+    {
+        CinReader sc = new CinReader();
+        while (true)
+        {
+            if (name.equalsIgnoreCase("candles"))
+            {
+                break;
+            }
+            else if (name.equalsIgnoreCase("crowbars"))
+            {
+                break;
+            }
+            else if (name.equalsIgnoreCase("rope"))
+            {
+                break;
+            }
+            else if (name.equalsIgnoreCase("torches"))
+            {
+                break;
+            }
+            else if (name.equalsIgnoreCase("Shovels"))
+            {
+                break;
+            }
+            else
+            {
+                System.out.println("Please enter a valid item" +
+                        "candles, crowbars, rope, torches shovels");
+                name = sc.readString(false);
+
+            }
+        }
+        return name;
+    }
+
+    private int calcWeight(String name)
+    {
+        int weight = 1;
+        return weight;
+    }
+
+    public int getWeight()
+    {
+        return this.weight;
+    }
 }
